aa83198a03c5eab7643f7a3704bd56f51f9eb9d6
Matthew Gidden <matthew.gidden@gmail.com>
2017-05-03 10:14:48 -0500

    update md5 function name

--- make_check_sums.py
+++ make_check_sums.py
@@ -2,7 +2,7 @@
 
 import glob
 
-from xarray.tutorial import _md5
+from xarray.tutorial import file_md5_checksum
 
 
 def main():
@@ -11,7 +11,7 @@ def main():
         prefix = ncf.split('.')[0]
         outf = '{}.md5'.format(prefix)
         with open(outf, 'w') as f:
-            f.write(_md5(ncf))
+            f.write(file_md5_checksum(ncf))
 
 if __name__ == '__main__':
     main()
